2bd14bafcb8c5ec8df9249f94c7f9cdb3a6f098a
Roman Turovskyy <roman.turovskyy@eleks.com>
2011-05-28 23:44:20 +0300

    code refactoring. Fixed issue with possible visiting of same directories. Errors are being written to Console.Error

---
+++ findfile/FilesFinder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace findfile
+{
+    public class FilesFinder
+    {
+        private readonly Dictionary<string, int> m_FilesCountMap =
+            new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+        private readonly HashSet<string> m_AlreadyVisitedDirs = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// This method does not traverse same directories twice even if user passes multiple same directories
+        /// </summary>
+        public IEnumerable<FileAndCount> SearchWithSameNames(IEnumerable<string> inDirectories)
+        {
+            if (inDirectories == null) throw new ArgumentNullException("inDirectories");
+            m_FilesCountMap.Clear();
+            m_AlreadyVisitedDirs.Clear();
+
+            foreach (string dir in inDirectories)
+            {
+                SearchRec(dir);
+            }
+
+            return GetWithSameNames();
+        }
+
+        private IEnumerable<FileAndCount> GetWithSameNames()
+        {
+            return m_FilesCountMap
+                .Where(pair => pair.Value > 1)
+                .Select(pair => new FileAndCount(pair.Key, pair.Value));
+        }
+
+        private void SearchRec(string dir)
+        {
+            if (!m_AlreadyVisitedDirs.Add(dir))
+            {
+                // such directory was already visited
+                return;
+            }
+
+            string[] files = Directory.GetFiles(dir);
+            foreach (var fullFileName in files)
+            {
+                string fileName = Path.GetFileName(fullFileName);
+                AddFile(fileName);
+            }
+
+            string[] subdirs = Directory.GetDirectories(dir);
+            foreach (var sub in subdirs)
+            {
+                SearchRec(sub);
+            }
+        }
+
+        private void AddFile(string fileName)
+        {
+            int count;
+            if(m_FilesCountMap.TryGetValue(fileName, out count))
+            {
+                m_FilesCountMap[fileName] = count + 1;
+            }
+            else
+            {
+                m_FilesCountMap.Add(fileName, 1);
+            }
+        }
+    }
+
+    public struct FileAndCount
+    {
+        public string FileName;
+        public int Count;
+
+        public FileAndCount(string fileName, int count)
+        {
+            FileName = fileName;
+            Count = count;
+        }
+    }
+}
--- findfile/Program.cs
+++ findfile/Program.cs
@@ -1,48 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace findfile
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             if (args.Length == 0)
             {
                 Usage();
-                return;
+                return 1;
             }
 
             if (!ValidateArguments(args))
             {
-                return;
+                return 1;
             }
 
             try
             {
-                var searcher = new FileSearcher();
-                var files = searcher.SearchWithSameNames(args);
-                foreach (var fileAndCount in files.OrderByDescending(f => f.Count))
-                {
-                    Console.WriteLine("{0}: {1}", fileAndCount.FileName, fileAndCount.Count);
-                }
+                DoSearch(args);
             }
             catch(Exception e)
             {
-                Console.WriteLine("Oops, error happened!");
-                Console.WriteLine("Dedails: {0}", e.Message);
+                Console.Error.WriteLine("Oops, error happened!");
+                Console.Error.WriteLine("Dedails: {0}", e.Message);
+            }
+
+            return 0;
+        }
+
+        private static void DoSearch(IEnumerable<string> args)
+        {
+            var filesFinder = new FilesFinder();
+            var filesWithSameName = filesFinder.SearchWithSameNames(args);
+            foreach (var fileAndCount in filesWithSameName)
+            {
+                Console.WriteLine("{0}: {1}", fileAndCount.FileName, fileAndCount.Count);
             }
         }
 
-        private static bool ValidateArguments(string[] args)
+        private static bool ValidateArguments(IEnumerable<string> args)
         {
             bool ok = true;
             foreach (var path in args)
             {
                 if (!Directory.Exists(path))
                 {
-                    Console.WriteLine("Error: directory '{0}' does not exist", path);
+                    Console.Error.WriteLine("Error: directory '{0}' does not exist", path);
                     ok = false;
                 }
             }
@@ -51,8 +58,10 @@ namespace findfile
 
         private static void Usage()
         {
-            Console.WriteLine("Usage");
-            Console.WriteLine("> findfile [-size] path1 [path2 pathN]");
+            Console.WriteLine(@"
+Use findfile to find files with the same name in different directories
+Usage:
+> findfile path1 [path2] [path3] [pathN]");
         }
     }
 }
